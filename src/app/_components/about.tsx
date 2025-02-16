b122ffe0c2d6c0d89c0bbe57260b3d6426f91c33
“AndreSamanhongagit config --global user.email “andrejeremias24@gmail.com <andrejeremias24@gmail.com>
2025-02-16 20:32:46 +0100

    actualização de formularios

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -61,7 +61,7 @@ export function About() {
             <div className="flex gap-2">
               <a
                 target='_blank'
-                href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações`}
+                href={`https://wa.me/244935267573?text=Olá vim pelo site e gostaria de mais informações sobre a Diocesana`}
                 className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
               >
                 <WhatsappLogo className="w-5 h-5 text-white" />
@@ -73,7 +73,7 @@ export function About() {
                 className=" flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
               >
                 <MapPin className="w-5 h-5 text-black" />
-                Endereço da loja
+                Endereço da Diocese
               </a>
             </div>
 
